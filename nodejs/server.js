82157c0d0f951e409652dfc5cd9f5a6fe5767bc0
Robson Santos Santana <robsonsanttos_21@hotmail.com>
2021-09-30 13:12:17 -0300

    Rotas e Parâmetros
    Aprendi como fazer requisições, utilizar rotas e os tipos de requisições

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+app.get('/users', (req, res) => {
+    const params = req.query;
+    console.log(params)
+    res.json(params)
+})
 
-app.get('/', (req, res) => {
+app.post('/users/:id', (req, res) => {
+    const params = req.params;
+    console.log(params)
+    res.json(params)
+})
 
-    res.send('Hello World!! ')
+app.post('/users', (req, res) => {
+    const params = req.body;
+    console.log(params)
+    res.json(params)
 })
 
 app.listen(3001);
