5701f7a05a688332392a19a2026680f21bacccdc
Robson Santos Santana <robsonsanttos_21@hotmail.com>
2021-10-01 15:23:24 -0300

    Finalização do CRUD e utilização do CORS
    Nesta etapa, Se finaliza a parte de backEnd. Aprendi a usar outros métodos para trabalhar com  banco

--- nodejs/src/routes.js
+++ nodejs/src/routes.js
@@ -1,24 +1,11 @@
 const express = require('express');
-const crypto = require('crypto');
-const connection = require('./database/connection');
 const routes = express.Router();
+const UserController = require('./controller/UserController');
 
-routes.get('/users', async(req, res) => {
-    const users = await connection('users').select('*');
-    res.json(users);
-})
-
-routes.post('/users', async(req, res) => {
-    const { name, email, idade, empresa } = req.body;
-    const id = crypto.randomBytes(4).toString('HEX');
-    await connection('users').insert({
-        id,
-        name,
-        email,
-        idade,
-        empresa
-    })
-    res.json({ id })
-})
+routes.get('/users', UserController.list)
+routes.get('/users/:id', UserController.show)
+routes.post('/users', UserController.create)
+routes.put('/users/:id', UserController.update)
+routes.delete('/users/:id', UserController.delete)
 
 module.exports = routes;
